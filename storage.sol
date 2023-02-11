a23757e54d571627f7b91874a3ea2b5a5e468620
kauser_bano <bkauser777@gmail.com>
2023-02-11 07:41:57 -0800

    storage

---
+++ storage.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.7.5;
+contract Datalocation{
+
+    uint number = 123;
+    string myname = "xyz";
+    uint mythirdnumber = 25;
+
+    function getString(string memory text, uint depoite) public pure returns(string memory){
+uint secondNumber = 5;
+text = "hello"
+string memory test = "This";
+return text;
+    }
+}
